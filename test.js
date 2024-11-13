45b517386fb1452ca3d7bc2153a9c86d9292e2f3
ThunderBoltC <chandon1072@gmail.com>
2024-11-13 23:47:02 +0600

    added

--- test.js
+++ test.js
@@ -16,3 +16,37 @@ let isLogged = false
 let ageF 
 console.table([name, age, isLogged, ageF])
 console.log(typeof age)
+
+//datatype conversions
+
+let score = "455"
+console.log(typeof score)
+
+let converted = score
+console.log(typeof converted)
+let faka= "not faka"
+converted = Boolean(faka)
+console.log(converted)
+
+//string to number conversion problem
+
+// ********* operations *************//
+console.log(2 + 2);
+let str1 = "chandan"
+let str2 = " hembrom"
+let str3
+str3 = str1 + str2
+console.log(str3)
+console.log("1" + 2 + 2) //122
+console.log(2 + 2 + "1") //41
+console.log("1"+(2+2))
+console.log(+true);
+
+let num1, num2, num3
+num1 = num2 = num3 = 10 + 7
+console.table([num1, num2, num3])
+let gameCounter = 100;
+gameCounter++
+console.log(gameCounter++);//101
+console.log(++gameCounter);//103
+console.log(gameCounter)//103
