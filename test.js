f6f64db1eca0df7274a92defd2ee0471a4100b04
ThunderBoltC <chandon1072@gmail.com>
2024-11-13 23:08:13 +0600

    yoyo

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-console.log("hello " + "2+2");
+const accountId = 14553
+let accoutEmail = "chandon1072@gmail.com"
+//accountId = 4//not allowed
+console.log(accountId)
+var accountPass = "abcd"
+accountCity = "dhaka"
+let cyd
+console.table([accountId, accountPass, accoutEmail, accountCity,cyd])
+/*
+never use var only use let
+*/
